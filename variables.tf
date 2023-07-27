1ca4d8d0bd34684eeaa833dd6d9923feb79138b2
Jorgeemu <42248567+Jorgeemu@users.noreply.github.com>
2023-07-27 16:33:30 -0600

    First push
    Downloaded terraform foundations files

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "billing_account" {
+  description = "The ID of the billing account to associate projects with"
+  type        = string
+  default     = "01268B-D8A5FD-D4649F"
+}
+
+variable "org_id" {
+  description = "The organization id for the associated resources"
+  type        = string
+  default     = "147812536333"
+}
+
+
+
